952295712bd02db535b294158f1ada2191065896
ralds <ronald.ralds@gmail.com>
2023-10-02 17:37:44 -0300

    ajuste autenticação

--- main.py
+++ main.py
@@ -2,28 +2,17 @@ import os
 from dotenv import load_dotenv
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from Src.Middleware.authentication import authorization_adm, authorization_group
+from Src.Middleware.authentication import authorization
 from Src.Controller.recolhimento_controller import handle_start_recolhimento, handle_stop_recolhimento, handle_status_recolhimento
 
 load_dotenv()
 
-version = "0.0.2"
-
 app = Client(
-    name=os.getenv("BOT_NAME_TELEGRAM"), 
+    name="RECOLHIMENTO_BOT", 
     api_hash=os.getenv("API_HASH_TELEGRAM"),
     api_id=os.getenv("API_ID_TELEGRAM"),
-    bot_token=os.getenv("BOT_TOKEN_TELEGRAM")
+    bot_token=os.getenv("BOT_TOKEN_TELEGRAM_RECOLHIMENTO")
     )
-
-chat_adm = [
-    int(os.getenv("CHAT_ID_ADM")),
-]
-
-chat_group = [
-    int(os.getenv("CHAT_ID_ADM")),
-    int(os.getenv("CHAT_ID_GROUP_RECOLHIMENTO")),
-]
 
 @app.on_message(filters.command("start"))
 def start(client, message: Message):
@@ -34,7 +23,7 @@ def start(client, message: Message):
 """)
 
 @app.on_message(filters.command("recolhimento"))
-@authorization_group(chat_group)
+@authorization()
 def financeiro(client, message: Message):
     message.reply_text(f"""
 /iniciar_recolhimento - Iniciar Recolhimento
@@ -43,43 +32,42 @@ def financeiro(client, message: Message):
 """)
 
 @app.on_message(filters.command("chatgroup"))
-@authorization_adm(chat_adm)
-def handle_chatgroup_id(client: Client, message: Message):
-    client.send_message(message.from_user.id, message)
+@authorization()
+def handle_group_id(client: Client, message: Message):
+    client.send_message(int(os.getenv("CHAT_ID_ADM")), message)
 
 @app.on_message(filters.command("chat"))
-def handle_chat_id(client: Client, message: Message):
-    text = f"{message.from_user.first_name}.{message.from_user.last_name} - ID:{message.from_user.id}"
+def handle__id(client: Client, message: Message):
+    text = f"{message.chat.first_name}.{message.chat.last_name} - ID:{message.from_user.id}"
     client.send_message(message.from_user.id, text)
-    if chat_adm[0] != message.from_user.id:
-        client.send_message(chat_adm[0], text)
+    if int(os.getenv("CHAT_ID_ADM")) != message.from_user.id:
+        client.send_message(int(os.getenv("CHAT_ID_ADM")), text)
 
 # iniciar cancelamento
 @app.on_message(filters.command("iniciar_recolhimento"))
-@authorization_group(chat_group)
+@authorization()
 def iniciar_recolhimento(client: Client, message: Message):
     handle_start_recolhimento(client, message)
 
 # parar cancelamento
 @app.on_message(filters.command("parar_recolhimento"))
-@authorization_group(chat_group)
+@authorization()
 def parar_recolhimento(client: Client, message: Message):
     handle_stop_recolhimento(client, message)
 
 # status cancelamento
 @app.on_message(filters.command("status_recolhimento"))
-@authorization_group(chat_group)
+@authorization()
 def status_recolhimento(client: Client, message: Message):
     handle_status_recolhimento(client, message)
 
 # stop service
 @app.on_message(filters.command("stop_service"))
-@authorization_adm(chat_adm)
+@authorization()
 def stop(client: Client, message: Message):
     print("Service Stopping")
     app.stop()
 
 print("Service Telegram Up!")
-print(f"Version {version}")
 app.run()
 
